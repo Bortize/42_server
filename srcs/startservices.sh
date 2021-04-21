1d2687255301db2a689b7eb4456d909d9a893cac
Borja <55995077+Bortize@users.noreply.github.com>
2021-04-21 19:45:24 +0200

    Update startservices.sh
    add comment

--- srcs/startservices.sh
+++ srcs/startservices.sh
@@ -7,4 +7,5 @@ echo "CREATE DATABASE wordpress;" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
+# Se queda el proceso corriendo en el background. Hace que no se cierre el contenedor
 tail -f /var/log/nginx/*.log
